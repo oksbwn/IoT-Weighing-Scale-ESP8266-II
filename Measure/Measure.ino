76da10dc2f62feff7f1b4400f23461649cbc1492
Main workstation @home (Bikash) <weargeniuslabs@gmail.com>
2019-01-04 00:47:04 +0530

    Code working

--- Measure/Measure.ino
+++ Measure/Measure.ino
@@ -10,12 +10,16 @@
 #define MQTT_PORT 1883
 
 //Calibration factor for HX711
-#define calibration_factor 11280.00 //This value is obtained using the caliberation sketch
-
+#define CAL_FACTOR 11280.00 //This value is obtained using the caliberation sketch
+#define CONV_FACTOR 0.453592
 #define DOUT 13
 #define CLK 15
 #define RED_L 12
 #define YELLOW_L 14
+
+#define _CON_DELAY 20000
+#define _THRESHOLD_WEIGHT 20
+#define _DETECTION_DELAY 7000
 
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
@@ -30,137 +34,103 @@ int detection_time = 0;
 double lastWeight;
 char publishValue[10];
 
+double measured_weight ;
+boolean break_weight_loop=true;
 
 void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.println("Connecting to Wi-Fi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  connectToMqtt();
+    Serial.println("Connected to Wi-Fi.");
+    connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-  wifiReconnectTimer.once(2, connectToWifi);
+    Serial.println("Disconnected from Wi-Fi.");
+    mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+    wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
+    Serial.println("Connecting to MQTT...");
+    mqttClient.connect();
 }
 
 void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
+    Serial.println("Connected to MQTT.");
+    Serial.print("Session present: ");
+    Serial.println(sessionPresent);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
+    Serial.println("Disconnected from MQTT.");
 
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
+    if (WiFi.isConnected()) {
+        mqttReconnectTimer.once(2, connectToMqtt);
+    }
 }
 
 void setup() {
-  scale.set_scale(calibration_factor);
-  scale.tare();
-  scale.power_down();
-  
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+    scale.set_scale(CAL_FACTOR);
+    scale.tare();
+    scale.power_down();
 
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+    pinMode(RED_L,OUTPUT);
+    pinMode(YELLOW_L,OUTPUT);
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();
 
-  connectToWifi();
-  
-  //Caliberation of HX711
+    wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+    connectToWifi();
+
+    //Caliberation of HX711
 }
 
 void loop() {
-  scale.power_up();
-  double measured_weight = scale.get_units(10) * 0.453592; //If you want average weight you can pass the num of iterations in get_units()
-  if (measured_weight > 20) {
+    scale.power_up();
+    measured_weight = scale.get_units(10) * CONV_FACTOR; //If you want average weight you can pass the num of iterations in get_units()
+    if (measured_weight > _THRESHOLD_WEIGHT && ((millis() - detection_time) > _CON_DELAY)) { //Checking millis to avoid repeated publish
 //      mqttClient.disconnect();
-      boolean break_weight_loop = true;
-      Serial.println("Weight detected.");
-      digitalWrite(RED_L, HIGH);
-      delay(500);
-      digitalWrite(RED_L, LOW);
-      detection_time = millis();
-      while (break_weight_loop) {
-        delay(10);
-        lastWeight = scale.get_units(10)* 0.453592;
-        if (lastWeight < 20) {
-          break_weight_loop = false;
-          digitalWrite(RED_L, HIGH);
-//        //PUSH Measured Weight
-          lastWeight = lastWeight + 2;
-          sprintf(publishValue, "%f", measured_weight);
-          yield();
-          mqttClient.publish("home/bathroom/scale/weight", 1, true, publishValue);
-       }
-       else
-        measured_weight=lastWeight;
-        if (((millis() - detection_time) > 15000)) {
-          break_weight_loop = false;
-          digitalWrite(YELLOW_L, HIGH);
-          //PUSH Measured Weight
-          sprintf(publishValue, "%f", lastWeight);
-          yield();
-          mqttClient.publish("home/bathroom/scale/weight", 1, true, publishValue);
+        break_weight_loop=true;
+        Serial.println("Weight detected.");
+        digitalWrite(RED_L, HIGH);
+        detection_time = millis();
+        while (break_weight_loop) {
+            delay(40);
+            lastWeight = scale.get_units()* CONV_FACTOR;
+            if (lastWeight < _THRESHOLD_WEIGHT) {
+                break_weight_loop = false;
+                digitalWrite(RED_L, LOW);
+            }
+            else
+                measured_weight=lastWeight;
+            if (((millis() - detection_time) > _DETECTION_DELAY)) {
+                break_weight_loop = false;
+                digitalWrite(RED_L, LOW);
+                digitalWrite(YELLOW_L, HIGH);
+            }
         }
-      }
-  }
-  scale.power_down();
-  delay(2000);
+        sprintf(publishValue, "%f", measured_weight);
+        yield();
+        mqttClient.publish("home/bathroom/scale/weight", 1, true, publishValue);
+    }
+    scale.power_down();
+    delay(2000);
+    analogWrite(YELLOW_L, 50);
+    if(((millis() - detection_time) > _CON_DELAY)) {
+        delay(500);
+        digitalWrite(YELLOW_L, LOW);
+    }
+
 }
 
